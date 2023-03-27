b3599a5721a119739dc6930f13ef3dc189eba1e4
obgnail <he1251698542@gmail.com>
2023-03-27 23:55:36 +0800

    add example

--- example/main.go
+++ example/main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	example1()
+	example2()
+}
+
+func example1() {
 	db := sqlite.NewDB()
 	err := db.Exec(`
 	CREATE TABLE user (
@@ -73,6 +78,58 @@ func main() {
 	for item := range tree.GetAllItems() {
 		fmt.Println(item)
 	}
+}
+func example2() {
+	table := &sqlite.Table{
+		Name:       "user",
+		PrimaryKey: "id",
+		Columns:    []string{"id", "sex", "age", "username", "email", "phone"},
+		Constraint: map[string]func(data string) error{
+			"id": sqlite.Compose(sqlite.IsInteger, sqlite.NotEmpty),
+			"sex": func(data string) error {
+				return sqlite.OptionLimit[string](sqlite.TrimQuotes(data), []string{"male", "female"})
+			},
+			"age":      sqlite.IsSignedInteger,
+			"username": func(data string) error { return sqlite.VarcharTooLong(data, 16) },
+			"email":    sqlite.IsString,
+			"phone":    sqlite.IsString,
+		},
+		Formatter: map[string]func(data string) interface{}{
+			"id":       sqlite.IntegerFormatter,
+			"sex":      sqlite.StringFormatter,
+			"age":      sqlite.IntegerFormatter,
+			"username": sqlite.StringFormatter,
+			"email":    sqlite.StringFormatter,
+			"phone":    sqlite.StringFormatter,
+		},
+		DefaultValue: []interface{}{"", "male", 0, "", "", ""},
+		Indies: map[string]*sqlite.BPTree{
+			"-": sqlite.NewBPTree(17, nil),
+		},
+	}
+
+	db := sqlite.NewDB()
+	db.AddTable(table)
+
+	for i := 1; i != 30; i++ {
+		sql := fmt.Sprintf(`INSERT INTO user (id, username, email) VALUES (%d, "userName-%d", "User-%d@gmail.com")`, i, i, i)
+		if err := db.Exec(sql); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	tree := db.Tables["user"].GetClusterIndex()
+
+	showTree(tree)
+
+	res := tree.Get(27)
+	fmt.Println(res)
+
+	result, err := db.Query(`SELECT email, id, username FROM user WHERE id > 20`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(result)
 }
 
 func showTree(tree *sqlite.BPTree) {
